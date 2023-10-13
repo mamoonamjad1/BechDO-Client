4c1d08f2595e913108cf46c32a502bc864eb5144
mamoonamjad17 <mamoon.amjad17@gmail.com>
2023-10-13 21:25:50 +0500

    updated

--- src/Seller/pages/Dashboard.jsx
+++ src/Seller/pages/Dashboard.jsx
@@ -6,10 +6,16 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import ButtonBase from '@mui/material/ButtonBase';
 import jwtDecode from 'jwt-decode';
-import { Box, Card, CardActions, CardContent, CardMedia, Button } from '@mui/material';
+import { Box, Card, CardActions, CardContent, CardMedia, Button, IconButton, Dialog, Slide,DialogTitle, DialogContent, DialogContentText, DialogActions,TextField,InputAdornment,MenuItem } from '@mui/material';
 import axios from 'axios';
 import { toast } from 'react-toastify';
-
+import DeleteIcon from '@mui/icons-material/Delete';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled";
+import ProductionQuantityLimitsIcon from "@mui/icons-material/ProductionQuantityLimits";
 const Img = styled('img')({
   margin: 'auto',
   display: 'block',
@@ -17,15 +23,29 @@ const Img = styled('img')({
   maxHeight: '100%',
 });
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 export default function DashBoard() {
   const token = localStorage.getItem("sellerToken");
   const decode = jwtDecode(token);
+  const [ productId,setProductId] = useState(null)
   const [data, setData] = React.useState([]);
   const productsPerPage = 6;
   const totalPages = Math.ceil(data.length / productsPerPage);
   const [currentPage, setCurrentPage] = React.useState(1);
   const [amount, setAmount] = useState(0);
   const [disabledItems, setDisabledItems] = React.useState([]);
+  const [open, setOpen] = React.useState(false);
+  const [categories, setCategories] = useState([]);
+  const [categoryName, setCategoryName] = useState([]);
+  const [name, setProductName] = useState("");
+  const [productDescription, setProductDescription] = useState("");
+  const [basePrice, setBasePrice] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [duration, setDuration] = useState("");
+  const [category, setCategory] = useState("");
+  const [categoryId, setCategoryId] = useState("");
 
   React.useEffect(() => {
     axios.get(`http://localhost:4000/product/get/${decode._id}`)
@@ -37,16 +57,25 @@ export default function DashBoard() {
       .catch(() => {
         toast.error("Couldn't Fetch Products");
       });
-  }, []);
+  }, [data]);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
 
+
   const startIndex = (currentPage - 1) * productsPerPage;
   const endIndex = startIndex + productsPerPage;
   const productsToDisplay = data.slice(startIndex, endIndex);
+ 
+  
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
   const handleAuctionStart = (productId, index) => {
     if (!disabledItems.includes(index)) {
       axios.post(`http://localhost:4000/auction/start/${productId}`)
@@ -58,6 +87,16 @@ export default function DashBoard() {
         toast.error("Error Starting Auction");
       });
     }
+  };
+  const handleAuctionDelete = (productId, index) => {
+      axios.delete(`http://localhost:4000/product/delete/${productId}`)
+      .then((res)=>{
+        toast.success("Product Deleted");
+      })
+      .catch(()=>{
+        toast.error("Error Deleting Product");
+      });
+    
   };
   useEffect(() => {
     // Fetch ad data for the logged-in user
@@ -79,6 +118,73 @@ export default function DashBoard() {
 
     fetchAdData();
   }, []);
+
+
+  const handleAuctionEdit = (productId) => {
+  setOpen(true);
+  setProductId(productId)
+};
+useEffect(() => {
+  axios
+    .get("http://localhost:4000/categories/get")
+    .then((res) => {
+      setCategories(res.data);
+      setCategoryName(res.data.map((category) => category.name));
+    })
+    .catch(() => {
+      console.log("Error Fetching");
+    });
+}, []);
+const handleCategoryChange = (event) => {
+  const selectedCategory = categories.find(
+    (category) => category.name === event.target.value
+  );
+  setCategory(event.target.value);
+  setCategoryId(selectedCategory ? selectedCategory._id : "");
+};
+
+const handleEdit = () => {
+
+  console.log("handleEdit called");
+
+  console.log("name:", name);
+  console.log("productDescription:", productDescription);
+  console.log("basePrice:", basePrice);
+  console.log("quantity:", quantity);
+  console.log("duration:", duration);
+  console.log("categoryId:", categoryId);
+  const formData = new FormData();
+  console.log("Update Form",formData)
+  formData.append("name", name);
+  formData.append("description", productDescription);
+  formData.append("basePrice", basePrice);
+  formData.append("quantity", quantity);
+  formData.append("duration", duration);
+  formData.append("category", categoryId); // Append the category ID
+
+  axios
+    .put(`http://localhost:4000/product/update-single/${productId}`,{
+      name,description:productDescription,basePrice,quantity,duration
+    })
+    .then((response) => {
+      console.log(response.data);
+      setProductName("");
+      setProductDescription("");
+      setBasePrice("");
+      setQuantity("");
+      setDuration("");
+      setCategory("");
+      
+      toast.success("Product Updated Successfully");
+      handleClose();
+    })
+    .catch((error) => {
+      console.error("Error submitting form:", error);
+    });
+};
+
+
+
 
   return (
     <>
@@ -183,14 +289,23 @@ export default function DashBoard() {
                   </Box>
                 </CardContent>
                 <CardActions>
-                  <Box display='flex' justifyContent='center' width="100%">
+                  <Box display='flex' justifyContent='flex-start' width="100%">
                     <Button
                       variant='contained'
                       onClick={() => handleAuctionStart(item._id, index)}
                       disabled={disabledItems.includes(index)}
+                      startIcon={<PlayArrowIcon />}
                     >
-                      Start Auction
+                      Auction
                     </Button>
+                  </Box>
+                  <Box display='flex' justifyContent='flex-end' >
+                    <IconButton onClick={() => handleAuctionDelete(item._id, index)}>
+                      <DeleteIcon />
+                    </IconButton>
+                    <IconButton  onClick={() => handleAuctionEdit(item._id, index)}>
+                      <ModeEditIcon />
+                    </IconButton>
                   </Box>
                 </CardActions>
               </Card>
@@ -211,6 +326,140 @@ export default function DashBoard() {
           ))}
         </Box>
       </Box>
+
+
+
+{/* EDIT FORM */}
+      <Dialog open={open} onClose={handleClose} TransitionComponent={Transition}>
+      
+        <DialogTitle >
+          Edit The Product Info
+        </DialogTitle>
+
+        <DialogContent>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sx={{mt:2}}>
+              <TextField
+                label="Product Name"
+                placeholder="Add Product Name"
+                fullWidth
+                value={name}
+                onChange={(e) => setProductName(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <DriveFileRenameOutlineIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Product Description"
+                placeholder="Add Product Description"
+                fullWidth
+                multiline
+                rows={4}
+                value={productDescription}
+                onChange={(e) => setProductDescription(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <DriveFileRenameOutlineIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Base Price"
+                fullWidth
+                placeholder="Auction will start from this price point."
+                value={basePrice}
+                onChange={(e) => setBasePrice(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MonetizationOnIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Quantity"
+                fullWidth
+                placeholder="Auction will last this amount of time"
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <ProductionQuantityLimitsIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Duration"
+                fullWidth
+                placeholder="Duration in seconds (Max 1 hour)"
+                value={duration}
+                onChange={(e) => setDuration(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <AccessTimeFilledIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Select Category"
+                fullWidth
+                select
+                value={category}
+                onChange={handleCategoryChange}
+                placeholder="Select Category"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <DriveFileRenameOutlineIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                variant="outlined"
+              >
+                {categoryName.map((name) => (
+                  <MenuItem key={name} value={name}>
+                    {name}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} variant='contained'>
+            Close
+          </Button>
+          <Button variant='contained' onClick={handleEdit}>
+            Edit
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
